9b5c0afc1dcc8e9fbcc30ba94a5c98d809cf8b17
AmriFikri <162122989+AmriFikri@users.noreply.github.com>
2024-05-13 19:45:15 +0700

    Add files via upload

---
+++ movie/src/main/java/DAOImplement/data_movieimplement.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package DAOImplement;
+import java.util.List;
+import movie.*;
+/**
+ *
+ * @author Lenovo
+ */
+public interface data_movieimplement {
+    public void insert(data_movie m);;
+    public void update(data_movie m);
+    public void delete(data_movie m);
+    public List<data_movie> getAll();
+}
---
+++ movie/src/main/java/main/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+import koneksi.connector;
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        main_view v = new main_view();
+        v.setVisible(true);
+        v.setLocationRelativeTo(null);
+    }
